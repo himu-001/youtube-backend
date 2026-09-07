c912bc3a1eeb099825c7574dded3f59d9e625664
himu-001 <khimanshukumar321@gmail.com>
2026-09-07 20:31:53 +0530

    add logout user and auth middleware

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -151,4 +151,27 @@ const loginUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  // we need to remove the user's refresh token from the database hence findByIdAndUpdate();
+  await User.findByIdAndUpdate(req.user._id,
+    {
+      $set: { refreshToken: undefined }
+    }, {
+      new: true
+    }
+  );
+
+  const options = {
+      httpOnly: true,
+      secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+
+})
+
+export { registerUser, loginUser, generate_Access_And_RefreshToken, logoutUser };
